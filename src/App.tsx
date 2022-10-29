4413968448f67b13fd105a45fdf376fd78fa1ed9
AryanTy20 <devty007@gmail.com>
2022-10-29 09:06:02 +0530

    style: tweaks

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,6 @@ import {
 } from "./components";
 import LazyImg from "./components/LazyImg";
 
-const url =
-  "https://images.unsplash.com/photo-1666844550308-9b47df48af49?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80";
-
 function App() {
   return (
     <>
