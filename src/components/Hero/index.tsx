ff3dbb041c383991bb26e97d2df0c4aecdc6a1b8
AryanTy20 <devty007@gmail.com>
2022-11-03 08:58:14 +0530

    feat: stagger animation added

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,22 +1,64 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { motion } from "framer-motion";
 import "./style.scss";
 
 const Hero = () => {
+  const [show, setShow] = useState(false);
+  useEffect(() => {
+    const interval = setInterval(() => setShow(true), 1000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  const heroVariants = {
+    hidden: { opacity: 0 },
+    show: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.3,
+      },
+    },
+  };
+
+  const items = {
+    hidden: {
+      opacity: 0,
+      y: 10,
+    },
+    show: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        // delay: 0.8,
+        duration: 0.3,
+      },
+    },
+  };
+
   return (
-    <section className="hero" id="main">
-      <h1>Hi, my name is </h1>
-      <h2>Aryan Tirkey.</h2>
-      <h3>I'm Fullstack Web Developer.</h3>
-      <p>
+    <motion.section
+      variants={heroVariants}
+      initial="hidden"
+      animate={show ? "show" : ""}
+      whileInView="visible"
+      viewport={{ once: true }}
+      className="hero"
+      id="main"
+    >
+      <motion.h1 variants={items}>Hi, my name is </motion.h1>
+      <motion.h2 variants={items}>Aryan Tirkey.</motion.h2>
+      <motion.h3 variants={items}>I'm Fullstack Web Developer.</motion.h3>
+      <motion.p variants={items}>
         I'm an ambitious self-taught Full Stack Developer / Freelancer with a
         strong desire to learn new things, expanding my skillset upto date.
         Learning how things works always passionate me.
-      </p>
-      <h4> Got idea's or Project to work on ?</h4>
-      <a href="mailto:aryantirk3y@gmail.com">
+      </motion.p>
+      <motion.h4 variants={items}>Got idea's or Project to work on ?</motion.h4>
+      <motion.a variants={items} href="mailto:aryantirk3y@gmail.com">
         <button>Contact Me</button>
-      </a>
-    </section>
+      </motion.a>
+    </motion.section>
   );
 };
 
